0ccb2ffb525aa31f63c8e1fd837f4d63434fbd40
SuperShekhar <shekhargyawali138@gmail.com>
2024-04-26 13:46:30 +0545

    Fead:made all the basic operation without dot and negative value and proper error management

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
 
-const display=document.querySelector(".display");
-function potulate(value){
-     display.textContent="";
-    let result=document.createElement("div");
-    result.textContent=value;
-    result.style.fontSize="90px";
-    display.append(result);
+const display = document.querySelector(".display");
+display.textContent = '';
+let final = 0, initial = 0, opera = "",result=0;
+
+function potulate(value) { //Function to display the result or number
+    display.textContent = value;
 }
-function operate(a1,b1,operator){
-    let a=parseInt(a1);
-    let b=parseInt(b1);
-    switch(operator){
-        case '+':return a+b;
-        case '-':return a-b;
-        case '*':return a*b;
-        case '/':return a/b;
-        case '%':return a%b;
+function operate(a1, b1, operator) { //function to do mathematical operation
+    let a = parseInt(a1);
+    let b = parseInt(b1);
+    switch (operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case '%': return a % b;
     };
-
 }
-let operator,final,initial;
 
-const ac=document.querySelector("#AC");
-ac.addEventListener("click",()=>{potulate("");
-initial=null;final=null;});
 
-const numbers=document.querySelectorAll(".numbers");
-numbers.forEach((number)=>{
-    number.addEventListener("click",()=>{potulate(number.textContent)})
+const ac = document.querySelector("#AC"); 
+ac.addEventListener("click", () => {  //action when AC button is pressed
+    potulate("");
+    initial = 0; final = 0; opera = "";
+});
+
+const numbers = document.querySelectorAll(".numbers");
+numbers.forEach((number) => {    //looping through each number and adding event listener and displaying when pressed
+    number.addEventListener("click", () => {
+
+        if (opera != "" && opera != "+/-"&&opera!=".") {
+            if (final === 0) { display.textContent = ""; }
+            final = display.textContent + number.textContent;
+            console.log(final)
+            potulate(final);
+        }
+
+        if (final === 0) {
+            if(initial===0)
+            display.textContent="";
+            initial = display.textContent + number.textContent;
+            console.log(initial)
+            potulate(initial);
+        }
+    })
 })
 
-
-const equal=document.querySelector("#equal");
-equal.addEventListener("click",()=>{
-let result=operate(initial,final,operator);
-potulate(result);
+const operators = document.querySelectorAll(".operator"); 
+operators.forEach((operator) => {             //looping through every operator and adding event listener and choosing the operator when pressed
+    operator.addEventListener("click", () => {
+        if(final!==0){
+            result = operate(initial, final, opera);
+            initial = result;
+            final = 0;
+            potulate(result);
+        }
+        opera = operator.textContent;
+        console.log(opera)
+    })
 });
+
+
+const equal = document.querySelector("#equal"); 
+equal.addEventListener("click", ()=>{  // action when equals button is clicked
+    
+    if(final!==0){
+    result = operate(initial, final, opera);
+    
+    final=0;
+    initial = result;
+    potulate(Math.round(result));
+    if(result===Infinity||result==NaN){
+        result=0;
+        initial=0;
+        opera="";
+    }
+}});
+
+
